21dd77b02bdc96659d2a50acac7a2c026b21452b
Minh Trang <minhtranglamthi@gmail.com>
2015-06-26 18:57:44 +0800

    lesson 1

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,1 +1,13 @@
-$("#main").append("Lam Thi Minh Trang");
+var formattedName = HTMLheaderName.replace("%data%", "Lam Thi Minh Trang");
+var formattedRole = HTMLheaderRole.replace("%data%", "Data Analyst");
+$("#header").append(formattedName, formattedRole);
+
+var bio = {
+	"name": "Trang",
+	"age": 24,
+	"contact": 83379702,
+	"picture": "C:\\Users\\MinhTrang-EZFX\\Desktop\\Minh Trang\\Background\\fun.jpg",
+	"message": "Welcome to my world!",
+	"skill": ["math", "web development", "excel"]
+	};
+$("#head").preppend(bio);
